997ebc5d1fe1949d9222b55e64d8f0bc9645af9f
mo <muha.11201120@gmail.com>
2023-02-03 22:36:35 +0300

    Adding functions to solutions_one.cpp file

--- solutions_one.cpp
+++ solutions_one.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include <string>
 using namespace std;
+/****** Enums ***************/
+enum enNumberType
+{
+    odd = 1,
+    Even = 2
+};
+/********* Functions *********/
+
+int readNumber()
+{
+    int number;
+    cout << "Enter A Number : ";
+    cin >> number;
+    return number;
+}
+enNumberType checkNumberType(int number)
+{
+    int result = number % 2;
+    if (result == 0)
+    {
+        return enNumberType::Even;
+    }
+    else
+    {
+        return enNumberType::odd;
+    }
+}
+void printNumberType(enNumberType number)
+{
+    if (number == enNumberType::Even)
+    {
+        cout << "The Number You Have Entered was Even" << endl;
+    }
+    else
+    {
+        cout << "The Number You Have Entered was Odd" << endl;
+    }
+}
+string readName()
+{
+    string name;
+    cout << "Enter Your Name : ";
+    getline(cin, name);
+    return name;
+}
+void printName(string name)
+{
+    cout << "\nYour Name iS => " << name << endl;
+}
+
 int main()
 {
-    cout << "Hello world..";
+    // printName(readName());
+    printNumberType(checkNumberType(readNumber()));
     return 0;
 }
