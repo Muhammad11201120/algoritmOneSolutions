2c3981b3e9aabb1e247cf1a7bd4e8710459ef6a1
mo <muha.11201120@gmail.com>
2023-02-04 06:46:41 +0300

    another functions to solutions_one.cpp file

--- solutions_one.cpp
+++ solutions_one.cpp
@@ -7,6 +7,16 @@ enum enNumberType
     odd = 1,
     Even = 2
 };
+/********** Structs ************/
+struct stInfo
+{
+    string firstName;
+    string lastName;
+    short age;
+    bool HasDrivingLicense;
+    bool HasRecommendation;
+};
+
 /********* Functions *********/
 
 int readNumber()
@@ -50,10 +60,64 @@ void printName(string name)
 {
     cout << "\nYour Name iS => " << name << endl;
 }
+stInfo readInfo()
+{
+    stInfo info;
+    cout << "Enter Your Age: ";
+    cin >> info.age;
+    cout << "Enter Your Driver License: ";
+    cin >> info.HasDrivingLicense;
+    cout << "Do You Have Any Recommendation: ";
+    cin >> info.HasRecommendation;
+    return info;
+}
+bool isAccepted(stInfo info)
+{
+    if (info.HasRecommendation)
+    {
+        return true;
+    }
+    else
+    {
+        return (info.age > 21 && info.HasDrivingLicense);
+    }
+}
+void printResult(stInfo info)
+{
+    if (isAccepted(info))
+        cout << "\nHired" << endl;
+    else
+        cout << "\nRejected" << endl;
+}
+stInfo readFullName()
+{
+    stInfo info;
+    cout << "Enter Your FirstName: ";
+    cin >> info.firstName;
+    cout << "Enter Your Second Name: ";
+    cin >> info.lastName;
 
+    return info;
+}
+string getFullName(stInfo name, bool reversed)
+{
+    string fullName;
+    if (reversed)
+        fullName = name.lastName + " " + name.firstName;
+    else
+        fullName = name.firstName + " " + name.lastName;
+
+    return fullName;
+}
+void printFullName(string fullName)
+{
+    cout << "Your Full Name is => " << fullName << endl;
+}
 int main()
 {
     // printName(readName());
-    printNumberType(checkNumberType(readNumber()));
+    // printNumberType(checkNumberType(readNumber()));
+    // printResult(readInfo());
+    printFullName(getFullName(readFullName(), true));
     return 0;
 }
