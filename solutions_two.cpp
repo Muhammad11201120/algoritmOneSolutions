6591b5db6e565627beccb8ed6fcddacc2ee871f4
mo <muha.11201120@gmail.com>
2023-02-05 22:02:57 +0300

    add some math functions to algorithm_two.cpp file

--- solutions_two.cpp
+++ solutions_two.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 void readNumbers(float &a, float &b)
 {
@@ -8,11 +9,16 @@ void readNumbers(float &a, float &b)
     cout << "Enter B Number: ";
     cin >> b;
 }
-float calculateTriangleArea(float a, float b)
+void readNumber(float &a)
+{
+    cout << "Enter A Number : ";
+    cin >> a;
+}
+float calculateRectangleArea(float a, float b)
 {
     return a * b;
 }
-void printTrangleArea(float result)
+void printRectangleArea(float result)
 {
     cout << "The Area Of The Rectangle is => " << result << endl;
 }
@@ -29,13 +35,73 @@ void swap(float &a, float &b)
     cout << "Number 1 After Swapping => " << a << endl;
     cout << "Number 2 After Swapping => " << b << endl;
 }
-using namespace std;
-
+float calculateRectangleAreaThroughDiagonal(float &a, float &b)
+{
+    return a * sqrt(b * b - a * a);
+}
+void printRectangleAreaThroughDiagonal(float area)
+{
+    cout << "The Area Of The Triangle is => " << area << endl;
+}
+float calculateTriangleArea(float &a, float &b)
+{
+    return a / 2 * b;
+}
+void printTriangleArea(float area)
+{
+    cout << "The Area Of Triangle is => " << area << endl;
+}
+float calculateCircleArea(float &a)
+{
+    return 3.14 * pow(a, 2);
+}
+void printCircleArea(float area)
+{
+    cout << "Area Of The Circle is => " << area << endl;
+}
+float readDiameter()
+{
+    float d;
+    cout << "Enter The Diameter: ";
+    cin >> d;
+    return d;
+}
+float calculateCircleAreaThroughDiameter(float d)
+{
+    return (3.14 * pow(d, 2)) / 4;
+}
+void printCircleAreaThroughDiameter(float area)
+{
+    cout << "The Area Of The Circle Through Diameter is => " << area << endl;
+}
+float readCircleAreaInscribedInASquare()
+{
+    float a;
+    cout << "Enter A of Square: ";
+    cin >> a;
+    return a;
+}
+float calculateCircleAreaInscribedInSquare(float a)
+{
+    return 3.14 * (pow(a / 2, 2));
+}
+void printCircleAreaInscribedInSquare(float area)
+{
+    cout << "Area Of The Circle is => " << area << endl;
+}
 int main()
 {
     float a, b; // here is a vars to send it by reffferance
     // readNumbers(a, b);
-    // printTrangleArea(calculateTriangleArea(a, b));
-    swap(a, b);
+    // printrectangleArea(calculateTriangleArea(a, b));
+    // swap(a, b);
+    // readNumbers(a, b);
+    // printRectangleAreaThroughDiagonal(calculateRectangleAreaThroughDiagonal(a, b));
+    // readNumbers(a, b);
+    // printTriangleArea(calculateTriangleArea(a, b));
+    // readNumber(a);
+    // printCircleArea(calculateCircleArea(a));
+    // printCircleAreaThroughDiameter(calculateCircleAreaThroughDiameter(readDiameter()));
+    printCircleAreaInscribedInSquare(calculateCircleAreaInscribedInSquare(readCircleAreaInscribedInASquare()));
     return 0;
 }
