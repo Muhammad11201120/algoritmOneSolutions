4e4e47e0af5559983c9194f153a8cc59c842532c
mo <muha.11201120@gmail.com>
2023-02-10 15:59:00 +0300

    Ading New Solutions

--- algorithm_2/solutions_two.cpp
+++ algorithm_2/solutions_two.cpp
@@ -38,7 +38,7 @@ void numberPattren(int number)
         cout << endl;
     }
 }
-void letterPattern(int number)
+void invertedLetterPattern(int number)
 {
     for (int i = number; i > 0; i--)
     {
@@ -49,11 +49,81 @@ void letterPattern(int number)
         cout << "\n";
     }
 }
+void letterPattern(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << char(65 - 1 + i);
+        }
+        cout << endl;
+    }
+}
+void wordsFromAtoZ()
+{
+    string word = "";
+    for (int i = 65; i <= 90; i++)
+    {
+        for (int j = 65; j <= 90; j++)
+        {
+            for (int k = 65; k <= 90; k++)
+            {
+                word += char(i);
+                word += char(j);
+                word += char(k);
+
+                cout << word << endl;
+                word = "";
+            }
+        }
+    }
+}
+string readString()
+{
+    string str;
+    cout << "Enter The String: ";
+    cin >> str;
+    return str;
+}
+void guessPassowrd(string pass)
+{
+    string word = "";
+    int counter = 0;
+    for (int i = 65; i <= 90; i++)
+    {
+        for (int j = 65; j <= 90; j++)
+        {
+            for (int k = 65; k <= 90; k++)
+            {
+                word = char(i);
+                word += char(j);
+                word += char(k);
+                counter++;
+                if (word == pass)
+                {
+                    cout << "Trial [" << counter << "]: ";
+                    cout << "The Pssword Is => " << word << endl;
+                    break;
+                }
+                cout << "Trial [" << counter << "]: => " << word << endl;
+                word = "";
+            }
+            if (word == pass)
+                break;
+        }
+        if (word == pass)
+            break;
+    }
+}
 /*********************Main*********************/
 int main()
 {
     // invertedPsttern(readPositiveNumber());
     // numberPattren(readPositiveNumber());
-    letterPattern(readPositiveNumber());
+    // invertedLetterPattern(readPositiveNumber());
+    // letterPattern(readPositiveNumber());
+    // wordsFromAtoZ();
+    guessPassowrd(readString());
     return 0;
 }
