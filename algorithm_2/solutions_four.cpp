1c4ce8742102ba75e0fa280bbb397a282c87b749
mo <muha.11201120@gmail.com>
2023-02-14 23:08:08 +0300

    new function to resolve some new problems

--- algorithm_2/solutions_four.cpp
+++ algorithm_2/solutions_four.cpp
@@ -24,7 +24,6 @@ void addUserNumberToArray(int arr[], int &arrLength)
         cin >> addMore;
     } while (addMore);
 }
-
 void getArray(int arr[], int indexes)
 {
     for (int i = 0; i < indexes; i++)
@@ -32,15 +31,94 @@ void getArray(int arr[], int indexes)
         cout << arr[i] << " ";
     }
 }
+int randNumber(int from, int to)
+{
+    int randNum = rand() % (to - from + 1) + from;
+    return randNum;
+}
+void readRandomElementsToArray(int arr[], int &arrLength)
+{
+    cout << "\nEnter Numnber Of Elements: \n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = randNumber(1, 100);
+    }
+}
+void copyArrayElements(int arr1[], int arr2[], int &arrLength, int &arra2Length)
+{
+    for (int i = 0; i < arrLength; i++)
+    {
+        addElementToArray(arr1[i], arr2, arra2Length);
+    }
+}
+void copyOddElementsToArray(int arr1[], int arr2[], int &arr1Length, int &arr2Length)
+{
+    for (int i = 0; i < arr1Length; i++)
+    {
+        if (arr1[i] % 2 != 0)
+        {
+            addElementToArray(arr1[i], arr2, arr2Length);
+        }
+    }
+}
+bool isPrimary(int number)
+{
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+        }
+    return true;
+}
+void copyPrimaryNumberToArray(int arr1[], int arr2[], int &arr1Length, int &arr2Length)
+{
+    for (int i = 0; i < arr1Length; i++)
+    {
+        if (isPrimary(arr1[i]))
+        {
+            addElementToArray(arr1[i], arr2, arr2Length);
+        }
+    }
+}
 /********************Main******************************/
 int main()
 {
-    int arr[50];
-    int arrLength = 0;
-    addUserNumberToArray(arr, arrLength);
+    srand((unsigned)time(NULL));
+    // int arr[50];
+    // int arrLength = 0;
+    // addUserNumberToArray(arr, arrLength);
 
-    cout << "Array Length => " << arrLength << endl;
-    cout << "Array Elements => ";
-    getArray(arr, arrLength);
+    // cout << "Array Length => " << arrLength << endl;
+    // cout << "Array Elements => ";
+    // getArray(arr, arrLength);
+
+    int arr1[50];
+    int arr2[50];
+
+    int arr1Length = 0;
+    int arr2Length = 0;
+
+    readRandomElementsToArray(arr1, arr1Length);
+
+    cout << "Array One Elements: " << endl;
+    getArray(arr1, arr1Length);
+
+    // copyArrayElements(arr1, arr2, arr1Length, arr2Length);
+    // copyOddElementsToArray(arr1, arr2, arr1Length, arr2Length);
+    copyPrimaryNumberToArray(arr1, arr2, arr1Length, arr2Length);
+
+    // cout << "\nArray Two Elements After Copy: " << endl;
+    // getArray(arr2, arr2Length);
+
+    // cout << "\nArray Two Odd Elements : " << endl;
+    // getArray(arr2, arr2Length);
+
+    cout << "\nArray Two Primary Elements : " << endl;
+    getArray(arr2, arr2Length);
+
     return 0;
 }
